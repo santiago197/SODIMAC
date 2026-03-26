1e3195d70b0997f6648595a663ee706aec395d73
Santiago Rodriguez <santinacional123.sr@gmail.com>
2026-03-26 16:41:20 -0500

    Page not found global

---
+++ not-found.tsx
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
+            <h1>404 - Página no encontrada</h1>
+            <p>Lo sentimos, la página que estás buscando no existe.</p>
+        </main>
+    )
+}
