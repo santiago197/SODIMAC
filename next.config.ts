8b73c409d73f5edab0b17b072073b997304aab84
Santiago Rodriguez <santinacional123.sr@gmail.com>
2026-03-26 11:42:41 -0500

    Configuración de imágenes para dominios y patrones remotos en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["media.falabella.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.falabella.com",
+        port: "",
+        pathname: "/sodimacCO/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
